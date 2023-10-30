7d9d856d7354df371e3c0b2d06a3ad46fb6553a8
Kyle Buchanan <kylebuch8@gmail.com>
2023-10-30 14:12:14 -0400

    adding a paragraph

--- app.vue
+++ app.vue
@@ -2,5 +2,6 @@
   <div>
     <h1>Nuxt.js proof-of-concept</h1>
     <h2>Delivered by OpenShift</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusantium ipsum perferendis repellendus reiciendis veniam nesciunt voluptates. Facilis ipsum aut similique molestias illum maiores, dolores ab unde a earum atque.</p>
   </div>
 </template>
