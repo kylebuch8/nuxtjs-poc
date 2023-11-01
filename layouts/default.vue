485655c509fc2f4d9e3c7da93d90eb7446f6a260
Kyle Buchanan <kylebuch8@gmail.com>
2023-11-01 16:17:30 -0400

    using the design system

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,33 @@
 <script setup>
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import AppHeader from '../components/AppHeader.vue';
 import AppFooter from '../components/AppFooter.vue';
 
 useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossorigin: '',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;700&family=Red+Hat+Text:wght@400;700&display=swap"'
+    }
+  ],
   script: [
     {
       type: 'importmap',
       innerHTML: `
 {
   "imports": {
-    "@patternfly/elements/": "https://ga.jspm.io/npm:@patternfly/elements@2.4.0/elements/",
-    "@rhds/elements/": "https://ga.jspm.io/npm:@rhds/elements@1.2.0/elements/"
+    "@patternfly/elements/": "https://ga.jspm.io/npm:@patternfly/elements@2.4.0/",
+    "@rhds/elements/": "https://ga.jspm.io/npm:@rhds/elements@1.2.0/elements/",
+    "@rhds/elements/rh-footer/rh-footer.js": "https://ga.jspm.io/npm:@rhds/elements@1.2.0/elements/rh-footer/rh-footer.js"
   },
   "scopes": {
     "https://ga.jspm.io/": {
@@ -20,7 +37,9 @@ useHead({
       "@floating-ui/utils/dom": "https://ga.jspm.io/npm:@floating-ui/utils@0.1.6/dom/dist/floating-ui.utils.dom.mjs",
       "@lit/reactive-element": "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/development/reactive-element.js",
       "@lit/reactive-element/decorators/": "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/development/decorators/",
+      "@patternfly/pfe-core": "https://ga.jspm.io/npm:@patternfly/pfe-core@2.4.1/core.js",
       "@patternfly/pfe-core/": "https://ga.jspm.io/npm:@patternfly/pfe-core@2.4.1/",
+      "@rhds/tokens/media.js": "https://ga.jspm.io/npm:@rhds/tokens@1.1.2/js/media.js",
       "lit": "https://ga.jspm.io/npm:lit@2.6.1/index.js",
       "lit-element/lit-element.js": "https://ga.jspm.io/npm:lit-element@3.3.3/development/lit-element.js",
       "lit-html": "https://ga.jspm.io/npm:lit-html@2.8.0/development/lit-html.js",
@@ -46,8 +65,37 @@ useHead({
 </template>
 
 <style>
+*,
+::after,
+::before {
+  box-sizing: border-box;
+}
+
 html,
 body {
   margin: 0;
+  font-size: 1.05rem;
+  font-family: 'Red Hat Text', sans-serif;
+  line-height: 30px;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: 'Red Hat Display', sans-serif;
+  font-weight: 300;
+}
+
+h1 {
+  font-size: 48px;
+  line-height: 62px;
+}
+
+h2 {
+  font-size: 40px;
+  line-height: 48px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -10,16 +10,72 @@ useHead({
   script: [
     {
       type: 'module',
-      innerHTML: 'import "@rhds/elements/rh-card/rh-card.js"'
+      innerHTML: `
+        import "@rhds/elements/rh-cta/rh-cta.js";
+        import "@rhds/elements/rh-card/rh-card.js";
+      `
     }
   ]
 })
 </script>
 
 <template>
-  <h1>Nuxt.js POC</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vero deserunt dolor saepe ut quae atque. Dignissimos omnis illo sed voluptatem. Debitis sit deserunt quod? Placeat alias quaerat quibusdam suscipit.</p>
-  <rh-card>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat alias porro obcaecati repudiandae. Perspiciatis vero magni, fugit deserunt consequatur accusamus veritatis similique reprehenderit voluptates ex exercitationem nemo nihil, harum distinctio.</p>
-  </rh-card>
+  <section class="hero">
+    <div class="container">
+      <div class="row pb-5">
+        <div class="col-lg-6">
+          <h1>More cloud choice.<br>Less cloud management.</h1>
+          <p>Build applications your way and launch on any cloud with Red Hat® OpenShift®—part of a portfolio of solutions that helps you defy constraints.</p>
+          <div class="d-md-flex">
+            <rh-cta class="mb-3 mb-md-0 me-md-3" variant="primary"><a href="https://www.redhat.com/en/technologies/cloud-computing/openshift/openshift-cloud-services">Explore OpenShift cloud services</a></rh-cta>
+            <rh-cta variant="secondary"><a href="https://www.redhat.com/en/solutions">Discover other solutions</a></rh-cta>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-9">
+          <h2>Red Hat is the leading provider of enterprise open source software solutions</h2>
+          <p>Enterprises around the world trust our broad portfolio of hybrid cloud infrastructure, application services, cloud-native application development, and automation solutions to deliver IT services on any infrastructure quickly and cost effectively.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-5">
+    <div class="container">
+      <h3>Build on a reliable foundation. Take your apps anywhere.</h3>
+      <div class="row">
+        <div class="col-lg-4">
+          <rh-card>
+            <h4 slot="header">Red Hat Enterprise Linux</h4>
+            <p>Support application deployments—from on premise to the cloud to the edge—in a flexible operating environment.</p>
+            <rh-cta slot="footer" variant="secondary"><a href="https://www.redhat.com/en/technologies/linux-platforms/enterprise-linux">Get product details</a></rh-cta>
+          </rh-card>
+        </div>
+        <div class="col-lg-4">
+          <rh-card>
+            <h4 slot="header">Red Hat OpenShift</h4>
+            <p>Quickly build and deploy applications at scale, while you modernize the ones you already have.</p>
+            <rh-cta slot="footer" variant="secondary"><a href="https://www.redhat.com/en/technologies/cloud-computing/openshift">Get product details</a></rh-cta>
+          </rh-card>
+        </div>
+        <div class="col-lg-4">
+          <rh-card>
+            <h4 slot="header">Red Hat Ansible Automation Platform</h4>
+            <p>Create, manage, and dynamically scale automation across your entire enterprise.</p>
+            <rh-cta slot="footer" variant="secondary"><a href="https://www.redhat.com/en/technologies/management/ansible">Get product details</a></rh-cta>
+          </rh-card>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.hero {
+  background-color: #feeded;
+}
+</style>
